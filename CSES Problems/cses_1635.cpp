eb990bdb79643b528a9d3db142e456cee02882d7
acidentally <iCombozstrafe@gmail.com>
2023-08-04 23:56:52 +0700

    lck da het ngu

--- CSES Problems/cses_1635.cpp
+++ CSES Problems/cses_1635.cpp
@@ -1,52 +1,55 @@
 /*
 Good luck for those who are trying your best
 May the most glorious victory come
+File name: cses_1635.cpp
 Code by : acident / lckintrovert
+Created since : 04/08/2023 ~~ 21:49:33
 Literally the worst cp-er ever
 */
-
 #include <bits/stdc++.h>
 using namespace std;
-
-#define int 			long long
-#define uint 			unsigned long long
-#define fi				first
-#define se 				second
-#define vvi 			vector< vector<int> >
-#define endl 			'\n'
-#define pb 				push_back
-#define pf 				push_front
-#define all(a) 			a.begin(), a.end()
-#define mp 				make_pair
-#define YES 			cout<<"YES\n";
-#define NO 				cout<<"NO\n";
-#define space   		' '
-#define ins 			insert
-typedef vector<int> 	vi;
-typedef pair<int, int> 	pi;
-int const mod 		= 	1e9 + 7;
-int const maxn 		= 	1e6 + 10;
-int const INF 		= 	LLONG_MAX;
-
+ 
+#define int                 long long
+#define uint                unsigned long long
+#define dub                 double
+#define fi                  first
+#define se                  second
+#define vvi                 vector< vector<int> >
+#define endl                '\n'
+#define pb                  emplace_back
+#define pf                  push_front
+#define all(a)              a.begin(), a.end()
+#define mp                  make_pair
+#define YES                 cout << "YES\n";
+#define NO                  cout << "NO\n";
+#define ins                 insert
+ 
+typedef vector<int>         vi;
+typedef pair<int, int>      pi;
+typedef pair<int, pi>       pii;
+int const mod       =       1e9 + 7;
+int const maxn      =       1e6 + 10;
+int const INF       =       1e18;
+ 
 int n, x;
-int a[200] = {}, dp[maxn] = {};
+int c[150] = {}, dp[maxn] = {};
 void solve() {
 	cin >> n >> x;
 	for(int i = 1; i <= n; i++) {
-		cin >> a[i];
-		dp[a[i]] = 1;
+		cin >> c[i];
+		dp[c[i]] = 1;
 	}
 	for(int i = 1; i <= x; i++) {
 		for(int j = 1; j <= n; j++) {
-			if(i - j < 1 || i - a[j] < 1) break;
-			dp[i] += (dp[i - a[j]]);
-			dp[i] %= mod;
+			if(i - c[j] < 0) break;
+			(dp[i] += dp[i - c[j]]) % mod;
 		}
 	}
-	cout << dp[1001] % mod;
+	cout << dp[x] % mod;
 }
 signed main() {
 	ios_base:: sync_with_stdio(0);
 	cin.tie(NULL); cout.tie(NULL);
- 	solve();
+	//File?
+	solve();
 }

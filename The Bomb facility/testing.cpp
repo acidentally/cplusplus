57a07ca9198fa3624c0b8706522d75a4d68568a4
acidentally <iCombozstrafe@gmail.com>
2023-08-04 13:50:56 +0700

    lck bị đần

--- The Bomb facility/testing.cpp
+++ The Bomb facility/testing.cpp
@@ -1,1 +1,43 @@
-cp
+/*
+Good luck for those who are trying your best
+May the most glorious victory come
+File name: testing.cpp
+Code by : acident / lckintrovert
+Created since : 04/08/2023 ~~ 13:04:32
+Literally the worst cp-er ever
+*/
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define int                 long long
+#define uint                unsigned long long
+#define dub                 double
+#define fi                  first
+#define se                  second
+#define vvi                 vector< vector<int> >
+#define endl                '\n'
+#define pb                  emplace_back
+#define pf                  push_front
+#define all(a)              a.begin(), a.end()
+#define mp                  make_pair
+#define YES                 cout << "YES\n";
+#define NO                  cout << "NO\n";
+#define ins                 insert
+ 
+typedef vector<int>         vi;
+typedef pair<int, int>      pi;
+typedef pair<int, pi>       pii;
+int const mod       =       1e9 + 7;
+int const maxn      =       1e5 + 10;
+int const INF       =       1e18;
+ 
+
+void solve() {
+    
+}
+signed main() {
+    ios_base:: sync_with_stdio(0);
+    cin.tie(NULL); cout.tie(NULL);
+    //File?
+    solve();
+}

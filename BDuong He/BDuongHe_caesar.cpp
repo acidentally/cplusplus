baa94f7f95e649ba64704fb6df950e27f34e12a4
acidentally <iCombozstrafe@gmail.com>
2023-08-04 10:01:00 +0700

    dyumb

--- BDuong He/BDuongHe_caesar.cpp
+++ BDuong He/BDuongHe_caesar.cpp
@@ -2,9 +2,9 @@
 Good luck for those who are trying your best
 May the most glorious victory come
 
-File name: BDuongHe_chiendich.cpp
+File name: BDuongHe_caesar.cpp
 Code by : acident / lckintrovert
-Created since : 2023-08-04 ~~ 09:30:23
+Created since : 2023-08-04 ~~ 08:47:32
 Literally the worst cp-er ever
 */
 #include <bits/stdc++.h>
@@ -28,44 +28,25 @@ using namespace std;
 typedef vector<int>         vi;
 typedef pair<int, int>      pi;
 typedef pair<int, pi>       pii;
-int const mod          =    1e9 + 7;
-int const maxn         =    1e3 + 10;
+int const mod          =    1e8;
+int const maxn         =    200;
 int const INF          =    LLONG_MAX;
 
-struct data {
-    int w;
-    double v, s;
-    data() : w(0), v(0), s(0) {}
-    data(int w_, int v_, int s_) : w(w_), v(v_), s(s_) {}
-};
-
-int n, p;
-double s;
-int w[maxn] = {};
-double v[maxn] = {};
-data dp[maxn][maxn] = {};   
+int n1, n2, k1, k2;
 void solve() {
-    cin >> n >> p >> s;
-    for(int i = 1; i <= n; i++) {
-        cin >> w[i] >> v[i];
-        if(w[i] > p) {
-            cout << -1;
-            return;
-        }
+    cin >> n1 >> n2 >> k1 >> k2;
+    k1++; k2++;
+    //compute (n1 + n2)! / n1! / n2! mod 1e8 là tổng sắp xếp n1 n2 thằng
+    //đếm xem có bao nhiêu permutation mà k1 thằng kia đứng cạnh, k2 thằng kia đứng cạnh
+    //rồi trừ cho phần giao của cả 2 
+    if(n1 < n2) swap(n1, n2);
+    int ans = 1, idx = 1;
+    for(int i = 51; i <= 100; i++) {
+        cerr << ans <<
+        ans = (ans * i / idx);
+        idx++;
     }
-    //Note : l/v -> t;
-    //Note 2 : dp[i][j] lưu tình trạng tốt nhất của state tại xe thứ i, sau khi đổi tốp j lần
-    dp[1][0] = data(w[1], v[1], 0);
-    for(int i = 2; i <= n; i++) {
-        for(int changes = 0; changes < n; changes++) {
-        
-        }
-    }
-    double ans = 10000.0;
-    for(int i = 1; i < n; i++) {
-        ans = min(ans, dp[n][i].t);
-    }
-    cout << fixed << setprecision(2) << ans;
+    cout << ans;
 }
 signed main() {
     ios_base:: sync_with_stdio(0);

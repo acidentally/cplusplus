a48e134d3abce727e03dc5e02fc2a7cb3275ca59
acidentally <iCombozstrafe@gmail.com>
2023-08-05 17:50:17 +0700

    lck bi ngu vcl

--- USACO practices/DP Knapsack/cses_1635.cpp
+++ USACO practices/DP Knapsack/cses_1635.cpp
@@ -1,9 +1,9 @@
 /*
 Good luck for those who are trying your best
 May the most glorious victory come
-File name: usaco_dp_phidias.cpp
+File name: cses_1635.cpp
 Code by : acident / lckintrovert
-Created since : 05/08/2023 ~~ 16:18:29
+Created since : 05/08/2023 ~~ 17:26:39
 Literally the worst cp-er ever
 */
 #include <bits/stdc++.h>
@@ -28,30 +28,27 @@ typedef vector<int>         vi;
 typedef pair<int, int>      pi;
 typedef pair<int, pi>       pii;
 int const mod       =       1e9 + 7;
-int const maxn      =       800;
-int const INF       =       1e18;
+int const maxn      =       1e6 + 3;
+// int const INF       =       1e18;
  
-int W, H, n;
-int dp[maxn][maxn] = {};
-int w[maxn] = {}, h[maxn] = {};
+int n, x, u;
+vi c;
+int dp[maxn];
 void solve() {
-    cin >> W >> H >> n;
-    for(int i = 1; i <= W; i++) for(int j = 1; j <= H; j++) dp[i][j] = i * j;
-    for(int i = 1; i <= n; i++) {
-        cin >> w[i] >> h[i];
-        dp[w[i]][h[i]] = 0;
+    cin >> n >> x;
+    while(n--) {
+        cin >> u;
+        c.pb(u);
     }
-    for(int i = 1; i <= W; i++) {
-        for(int j = 1; j <= H; j++) {
-            for(int k = 1; k <= i; k++) {
-                dp[i][j] = min(dp[i][j], dp[i - k][j] + dp[k][j]);
-            }
-            for(int k = 1; k <= j; k++) {
-                dp[i][j] = min(dp[i][j], dp[i][j - k] + dp[i][k]);
-            }
+    sort(all(c));
+    dp[0] = 1;
+    for(int i = 1; i <= x; i++) {
+        for(int j : c) {
+            if(i - j < 0) break;
+            (dp[i] += dp[i - j]) %= mod;
         }
     }
-    cout << dp[W][H] << endl;
+    cout << (dp[x] % mod);
 }
 signed main() {
     ios_base:: sync_with_stdio(0);

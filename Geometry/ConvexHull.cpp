5eecc9e4ed5bea80da8460d7ee3c3b3661cecdec
acidentally <iCombozstrafe@gmail.com>
2023-09-23 09:47:49 +0700

    ncc

--- Geometry/ConvexHull.cpp
+++ Geometry/ConvexHull.cpp
@@ -1,9 +1,9 @@
 /*
 Good luck for those who are trying your best
 May the most glorious victory come
-File name: Bai1.cpp
+File name: ConvexHull.cpp
 Code by : acident / lckintrovert
-Created since : 21/09/2023 ~~ 21:50:13
+Created since : 23/09/2023 ~~ 07:49:36
 Literally the worst cp-er ever
 */
 #include <bits/stdc++.h>
@@ -38,38 +38,69 @@ typedef vector<int>          vi;
 typedef vector<vi>           vvi;
 typedef vector<pi>           vp;
 const int mod       =        1e9 + 7;
-const int maxn      =        2e5 + 10;
+const int maxn      =        1e5 + 10;
 const int INF       =        1e18;
 
-int n, k, ans = 0;
-int a[maxn] = {}, dp[maxn] = {}, mini[maxn] = {};
-deque<int> q;
+struct point {
+    int x = 0, y = 0;
+    point() = default;
+    point(int x_, int y_) : x(x_), y(y_) {}
+};
+struct vec {
+    int x, y;
+    vec() = default;
+    vec(point a, point b) {
+        x = b.x - a.x;
+        y = b.y - a.y;
+    }
+
+};
+bool ccw(point a, point b, point c) {
+    vec v1 = vec(a, b);
+    vec v2 = vec(a, c);
+    return ((v1.x * v2.y - v1.y * v2.x) > 0);
+}
+int n, u, v;
+vector<point> e, hull;
 inline void solve() {
-    cin >> n >> k;
+    cin >> n;
     for(int i = 1; i <= n; i++) {
-        cin >> a[i];
+        cin >> u >> v;
+        e.pb(point(u, v));
     }
-    for(int i = 1, cur = a[1]; i <= k; i++) {
-        dp[i] = 0;
-        minimize(cur, a[i]);
-        if(i == k) dp[k] = cur;
+    sort(all(e), [](const point &a, const point &b) {
+        if(a.x != b.x) return a.x < b.x;
+        return a.y < b.y;
+    });
+    hull.pb(e[0]);
+    for(int i = 1; i < n; i++) {
+        // cerr << s.x << ' ' << s.y << endl;
+        while(hull.size() >= 2 && ccw(hull[hull.size() - 2], hull.back(), e[i]))
+            hull.pop_back();
+        hull.pb(e[i]);
     }
-    for(int i = 1; i <= n; i++) {
-        while(!q.empty() && a[q.back()] >= a[i]) q.pop_back();
-        q.push_back(i);
-        if(q.front() + k <= i) q.pop_front();
-        if(i >= k) mini[i] = a[q.front()];
+    // cerr << "CUR HULL: ";
+    // for(auto s : hull) {
+    //     cerr << s.x << ' ' << s.y << " --- ";
+    // }
+    // cerr << endl;
+    for(int i = e.size() - 2; i >= 0; i--) {
+        // cerr << e[i].x << ' ' << e[i].y << endl;
+        while(hull.size() >= 2 && ccw(hull[hull.size() - 2], hull.back(), e[i])) 
+            hull.pop_back();
+        hull.pb(e[i]);
     }
-    for(int i = k; i <= n; i++) {
-        dp[i] = max(dp[i - 1], mini[i] + dp[i - k]);
-        maximize(ans, dp[i]);
-    }
-    cout << ans;
+    if(n > 1) hull.pop_back();
+    cout << hull.size() << endl;
+    for(auto s : hull) cout << s.x << ' ' << s.y << endl;
 }
 signed main() {
     ios_base:: sync_with_stdio(0);
     cin.tie(NULL); cout.tie(NULL);
-    //File?
+    // #ifdef ONLINE_JUDGE
+    // freopen("test.INP", "r", stdin);
+    // freopen("test.OUT", "w", stdout);
+    // #endif //ONLINE JUDGE
     solve();
 }
 
